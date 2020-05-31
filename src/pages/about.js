da04e43b7970d64fa866daae62fa73b38d4d2bd4
Davis <[agdaemon@gmail.com]>
2020-06-01 01:49:42 +0800

    gist

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+// Page コンポーネント
+export default () => (
+  <div>
+    <h1>こんにちは〜</h1>
+    <h2>マッカートニー</h2>
+  </div>
+)
